ba716ba269dd36cd7819502b8fb30b8ae8fa48f3
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2021-11-30 10:40:06 -0500

    Merge 2

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="Davigamer987"
+export GITHUB_EMAIL="davimarchesiprimo@gmail.com"
 
-export device=""
+export device="TP1803"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+# export ROM=""
+export ROM_DIR="android"
+export ROM_VERSION="a12"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
-export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export local_manifest_url="https://raw.githubusercontent.com/davigamer987/local_manifests/main/TP1803/A12-Lineage.xml"
+# export manifest_url=""
+# export rom_vendor_name=""
+# export branch=""
+# export bacon="bacon"
+export buildtype="userdebug"
+export clean="false"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="50"
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="davigamer987/releases"
 
-export timezone="UTC"
+export timezone="EST"
