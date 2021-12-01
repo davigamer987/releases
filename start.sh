c251dd65ce5a2b591a7c82a5a60e1a84ae298391
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2021-12-01 06:07:27 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ export rom_vendor_name=$x_vendor_name
 read -p "Enter Branch Name : " x_branch_name
 export branch=$x_branch_name
 read -p "Enter Rom Name : " x_rom_name
-export rom=$x_rom_name
+export ROM=$x_rom_name
 export ROM_VERSION=$x_rom_name A12
 read -p "Enter Make Command Type (Eg, bacon) : " x_make_type
 export bacon=$x_make_type
