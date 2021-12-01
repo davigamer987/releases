ac2799258d016f533e3ee441a93f3c5e715b203a
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2021-12-01 06:11:58 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ read -p "Enter Branch Name : " x_branch_name
 export branch=$x_branch_name
 read -p "Enter Rom Name : " x_rom_name
 export ROM=$x_rom_name
-export ROM_VERSION=$x_rom_name A12
+export ROM_VERSION=A12
 read -p "Enter Make Command Type (Eg, bacon) : " x_make_type
 export bacon=$x_make_type
 echo "Starting real build now Sur"
