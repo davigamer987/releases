7aeecf810270813465e91355d96cff2d2bfbbab8
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2021-12-21 21:28:51 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,6 @@
 export my_dir=$(pwd)
 read -p "Enter Manifest Url: " x_manifest_url
 export manifest_url=$x_manifest_url
-read -p "Enter Rom Vendor Name : " x_vendor_name
-export rom_vendor_name=$x_vendor_name
 read -p "Enter Branch Name : " x_branch_name
 export branch=$x_branch_name
 read -p "Enter Rom Name : " x_rom_name
