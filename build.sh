243dbb4a45ced17f6f06ada03a45c08e4f2f439e
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2022-04-13 13:26:50 -0400

    Fix percentage (Thanks @phhusson for helping )

--- build.sh
+++ build.sh
@@ -42,12 +42,11 @@ buildsuccessful="${?}"
 
 while [ "$buildon" = "true" ]
 do  
-   export num1=$(protoc --decode_raw < ~/releases/android/out/build_progress.pb | cut -c 4- | head -1)
-   export num2=$(protoc --decode_raw < ~/releases/android/out/build_progress.pb | cut -b 4-  | head -n 2 | tail -n 1)
-   echo "scale=2 ; $num1 / $num2*100" | bc > tmpmath
-   export per=$(cat tmpmath)
+   num1=$(protoc --decode_raw < ~/releases/android/out/build_progress.pb | cut -c 4- | head -1)
+   num2=$(protoc --decode_raw < ~/releases/android/out/build_progress.pb | cut -b 4-  | head -n 2 | tail -n 1)
+   export per="$(echo "scale=2; $num1 / $num2*100" | bc)"
    curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data message_id=553 --data text=$per+% --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/editMessageText
-   rm tmpmath
+   [ "$num1" = "$num2" ] && break
   sleep 170  
 done
 export buildon=false
