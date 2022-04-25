ca525c96fa634db513fac7150685a729e53c5256
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2022-04-24 21:52:21 -0400

    Fix Config

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 export GITHUB_USER="Davigamer987"
 export GITHUB_EMAIL="davimarchesiprimo@gmail.com"
 
-export device="TP1803"
+export device="guacamole"
 
 # export ROM=""
 export ROM_DIR="android"
 # export ROM_VERSION="a12"
 export official="false"
-export local_manifest_url="https://raw.githubusercontent.com/SirRGB/local_manifests/main/TP1803/A12Lineage.xml"
+export local_manifest_url="https://raw.githubusercontent.com/davigamer987/local_manifests/main/guacamole/A12Lineage.xml"
 # export manifest_url=""
 # export rom_vendor_name=""
 # export branch=""
