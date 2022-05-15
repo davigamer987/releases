f9856b8d31afc3326842d14678bbf89c4dab9c53
davigamer987 <68574209+davigamer987@users.noreply.github.com>
2022-05-15 13:07:56 -0400

    Fix message spam

--- per.sh
+++ per.sh
@@ -1,7 +1,8 @@
+export tgid=$(curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data message_id=553 --data text=Waiting --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage | grep -Eo '"message_id".*"sender_chat"' | grep -Eo '[0-9]{1,4}')
+sleep 20
+
 while true
 do  
-   sleep 80
-   export tgid=$(curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data message_id=553 --data text=Waiting --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendMessage | grep -Eo '"message_id".*"sender_chat"' | grep -Eo '[0-9]{1,4}')
    num1=$(protoc --decode_raw < ~/releases/android/out/build_progress.pb | cut -c 4- | head -1)
    num2=$(protoc --decode_raw < ~/releases/android/out/build_progress.pb | cut -b 4-  | head -n 2 | tail -n 1)
    export per="$(echo "scale=2; $num2 / $num1*100" | bc)"
